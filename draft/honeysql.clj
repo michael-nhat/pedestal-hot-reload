1bd08c47f5e679eec8322c783dfa077683448aab
michael-nhat <nhathoang.haiphong@gmail.com>
2023-01-19 18:36:20 +0700

    db draft

--- draft/honeysql.clj
+++ draft/honeysql.clj
@@ -3,15 +3,30 @@
             [hugsql.core :as hug]
             [toucan.db :as db]
             [toucan.models :as model]
-            []))
+            [clojure.java.jdbc :as j]))
 
 (db/set-default-db-connection!
  {:classname "org.postgresql.Driver"
   :subprotocol "postgresql"
-  :subname "//localhost:5432/toucan_db"
-  :user "cam"})
+  :subname "//localhost:5432/shop_udemy"
+  :user "postgres"
+  :password "postgres"})
 
-(model/defmodel User :user_table)
 
-(db/select-one-field :first-name User)
+(def pg-db {:dbtype "postgresql"
+            :port 5432
+            :user "postgres"
+            :dbname "shop_udemy"
+            :password "postgres"})
+
+
+(j/query pg-db ["SELECT current_database()"])
+
+
+
+(model/defmodel Book :book)
+
+(db/select- ['Database :name] :id )
+
+(Book 1)
 
